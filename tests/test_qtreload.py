e8717503680833382faf4a0322d384d7e4431161
Lukasz Migas <lukas.migas@yahoo.com>
2023-03-07 16:30:29 +0100

    Update test_qtreload.py

--- tests/test_qtreload.py
+++ tests/test_qtreload.py
@@ -1,7 +1,8 @@
+import os
 from pathlib import Path
 
 import pytest
-from qtreload.qt_reload import QtReloadWidget, get_import_path, path_to_module
+from qtreload.qt_reload import QtReloadWidget, get_import_path, path_to_module, install_hot_reload
 
 
 @pytest.mark.parametrize(
@@ -39,6 +40,26 @@ def test_widget(qtbot):
     qtbot.addWidget(widget)
     assert widget is not None
     assert len(widget.path_to_index_map) > 0, "Expected more than 1 path"
+
+    # make sure we can retrieve each module properly
+    for path in widget.path_to_index_map.keys():
+        module = path_to_module(path, widget.get_module_path_for_path(path))
+        assert module, "Module should not be empty"
+
+
+def test_install_hot_reload(qtbot):
+    """Test installing hot reload."""
+    os.environ["QTRELOAD_HOT_RELOAD"] = "0"
+    widget = install_hot_reload(None)
+    assert widget is None
+
+    os.environ["QTRELOAD_HOT_RELOAD"] = "1"
+    os.environ["QTRELOAD_HOT_RELOAD_MODULES"] = "qtreload, superqt"
+    widget = install_hot_reload(None)
+    qtbot.addWidget(widget)
+    assert widget is not None
+    assert len(widget.path_to_index_map) > 0, "Expected more than 1 path"
+    assert len(widget._module_paths) == 2, "Expected 2 modules"
 
     # make sure we can retrieve each module properly
     for path in widget.path_to_index_map.keys():
