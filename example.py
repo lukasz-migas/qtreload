90d545c096df3a50aac8411f56d41aecc1a0f982
Lukasz Migas <lukas.migas@yahoo.com>
2025-02-10 15:47:29 +0000

    fixes

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-"""SampleWidget that contains many types of QWidgets.
+®"""SampleWidget that contains many types of QWidgets.
 
 This file and SampleWidget is useful for testing out themes from the command
 line or for generating screenshots of a sample widget to demonstrate a theme.
